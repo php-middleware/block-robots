05c154703d94862be41ebe6a5fc31ab8f3b15b55
Witold Wasiczko <witold@wasiczko.pl>
2017-05-12 00:18:19 +0200

    Add support for psr-11

--- src/BlockRobotsMiddleware.php
+++ src/BlockRobotsMiddleware.php
@@ -2,24 +2,28 @@
 
 namespace PhpMiddleware\BlockRobots;
 
-use Psr\Http\Message\ResponseInterface;
+use Interop\Http\ServerMiddleware\DelegateInterface;
+use Interop\Http\ServerMiddleware\MiddlewareInterface;
+use PhpMiddleware\DoublePassCompatibilityTrait;
 use Psr\Http\Message\ServerRequestInterface;
 use Zend\Diactoros\Response;
 
-class BlockRobotsMiddleware
+class BlockRobotsMiddleware implements MiddlewareInterface
 {
+    use DoublePassCompatibilityTrait;
+
     const ROBOTS_HEADER = 'X-Robots-Tag';
 
-    public function __invoke(ServerRequestInterface $request, ResponseInterface $response, callable $out = null)
+    public function process(ServerRequestInterface $request, DelegateInterface $delegate)
     {
         if ($request->getUri()->getPath() === '/robots.txt') {
-            $newReponse = new Response('php://memory', 200, ['Content-Type' => 'text/plain']);
-            $newReponse->getBody()->write("User-Agent: *\nDisallow: /");
+            $response = new Response('php://memory', 200, ['Content-Type' => 'text/plain']);
+            $response->getBody()->write("User-Agent: *\nDisallow: /");
 
-            return $newReponse;
+            return $response;
         }
-        /* @var $out ResponseInterface */
-        $response = $out === null ? $response : $out($request, $response);
+
+        $response = $delegate->process($request);
 
         return $response->withHeader(self::ROBOTS_HEADER, 'noindex, nofollow');
     }
