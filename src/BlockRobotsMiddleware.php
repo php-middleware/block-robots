d4d7d59ed9aade5af0315ee6bcfcc72bb6a49d40
Witold Wasiczko <witold@wasiczko.pl>
2017-05-12 00:41:45 +0200

    Improve readability

--- src/BlockRobotsMiddleware.php
+++ src/BlockRobotsMiddleware.php
@@ -5,6 +5,7 @@ namespace PhpMiddleware\BlockRobots;
 use Interop\Http\ServerMiddleware\DelegateInterface;
 use Interop\Http\ServerMiddleware\MiddlewareInterface;
 use PhpMiddleware\DoublePassCompatibilityTrait;
+use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Zend\Diactoros\Response;
 
@@ -16,15 +17,30 @@ class BlockRobotsMiddleware implements MiddlewareInterface
 
     public function process(ServerRequestInterface $request, DelegateInterface $delegate)
     {
-        if ($request->getUri()->getPath() === '/robots.txt') {
-            $response = new Response('php://memory', 200, ['Content-Type' => 'text/plain']);
-            $response->getBody()->write("User-Agent: *\nDisallow: /");
-
-            return $response;
+        if ($this->isRobotsTxt($request)) {
+            return $this->createRobotsTxtResponse();
         }
 
         $response = $delegate->process($request);
 
+        return $this->attachRobotsHeaderTo($response);
+    }
+
+    private function createRobotsTxtResponse()
+    {
+        $response = new Response('php://memory', 200, ['Content-Type' => 'text/plain']);
+        $response->getBody()->write("User-Agent: *\nDisallow: /");
+
+        return $response;
+    }
+
+    private function isRobotsTxt(ServerRequestInterface $request)
+    {
+        return $request->getUri()->getPath() === '/robots.txt';
+    }
+
+    private function attachRobotsHeaderTo(ResponseInterface $response)
+    {
         return $response->withHeader(self::ROBOTS_HEADER, 'noindex, nofollow');
     }
 }
